c1f5c2dd3336bdbbc9ccb13a30e6d91ec917344a
ArkhipovES <arhipoves@mchs.ru>
2020-11-12 23:36:38 +0300

    rm contact model from controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -64,7 +64,6 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
-
     /**
      * Displays contact page.
      *
@@ -72,15 +71,7 @@ class SiteController extends Controller
      */
     public function actionContact()
     {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
+        return $this->render('contact');
     }
 
     /**
